621a7fbf7000fc2e51478bb8ac845f0ee61aef78
Cristian Felipe Hernandez Zuluaga <chernandez@ibm.com>
2023-08-16 09:36:18 -0500

    ReactiveFormsModule added to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule // importamos el modulo de formularios reactivos
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gen/gen.module.ts
+++ src/app/gen/gen.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { GenRoutingModule } from './gen-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, GenRoutingModule],
+  imports: [CommonModule, GenRoutingModule, ReactiveFormsModule],
 })
 export class GenModule {}
