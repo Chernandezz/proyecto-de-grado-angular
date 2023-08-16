4a805c9ff9e8a498a34a3ef75dcfeaa2c39e7631
Cristian Felipe Hernandez Zuluaga <chernandez@ibm.com>
2023-08-16 09:37:12 -0500

    The html was adapted to the reactive forms and also the home component.

--- src/app/gen/pages/home/home.component.ts
+++ src/app/gen/pages/home/home.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'gen-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  title = 'gen works!';
-  
+  formularioInicialAlgoritmo = this.fb.group({
+    funcion: ['', Validators.required],
+    tipoSeleccion: ['', Validators.required],
+    tipoCruce: ['', Validators.required],
+    tipoMutacion: ['', Validators.required],
+    probCruce: [0.8, Validators.required],
+    probMutacion: [0.1, Validators.required],
+    numDecimales: 2,
+    elitisimo: false,
+    convergencia: false,
+    numGeneraciones: 1000,
+    numIndividuos: 1000,
+    xMin: 0,
+    xMax: 10,
+    tituloEjecucion: '',
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log(this.formularioInicialAlgoritmo.value);
+  }
 }
