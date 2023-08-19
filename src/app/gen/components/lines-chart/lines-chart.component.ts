b6a95baddb0821161da785bbb7e3f0e3955f0e57
Cristian Felipe Hernandez Zuluaga <chernandez@ibm.com>
2023-08-19 13:22:28 -0500

    Added charts component

---
+++ src/app/gen/components/lines-chart/lines-chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gen-lines-chart',
+  templateUrl: './lines-chart.component.html',
+  styles: [
+  ]
+})
+export class LinesChartComponent {
+
+}
--- src/app/gen/gen.module.ts
+++ src/app/gen/gen.module.ts
@@ -4,9 +4,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { GenRoutingModule } from './gen-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { GraphicsComponent } from './pages/graphics/graphics.component';
+import { LinesChartComponent } from './components/lines-chart/lines-chart.component';
 
 @NgModule({
-  declarations: [HomeComponent, GraphicsComponent],
+  declarations: [HomeComponent, GraphicsComponent, LinesChartComponent],
   imports: [CommonModule, GenRoutingModule, ReactiveFormsModule],
 })
 export class GenModule {}
